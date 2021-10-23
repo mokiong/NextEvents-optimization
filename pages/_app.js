41ef5a814d49501d7398a3d351871a3153016c09
Moki <mokiong1427@gmail.com>
2021-10-23 15:04:54 +0800

    Add global header

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
+import Head from 'next/head';
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Head>
+                <title>Next Events</title>
+                <meta name="description" content="NextJS events" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0"
+                    width="device-width"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
